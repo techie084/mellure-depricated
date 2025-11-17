7a09765d93bf4c3e940979a5c206c2f848f442e7
Wisdom Matthew <wisdommatthew08@gmail.com>
2025-11-17 01:51:35 +0100

    force push latest working version

--- src/routes/api/user/+server.ts
+++ src/routes/api/user/+server.ts
@@ -2,7 +2,7 @@ import { json } from '@sveltejs/kit';
 import { db } from '$lib/server/db.js';
 import { usersTable } from '$lib/server/schema.js';
 
-export const GET = async ({ request }) => {
+export const GET = async () => {
 	const user = await db.select().from(usersTable);
 
 	return json(user, { status: 200 });
